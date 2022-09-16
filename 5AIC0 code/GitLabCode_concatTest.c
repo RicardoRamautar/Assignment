a8a83f0821c25b694de06660dd579f83bb5ed750
RicardoRamautar <80088597+RicardoRamautar@users.noreply.github.com>
2022-09-16 16:43:56 +0200

    changed things

--- 5AIC0 code/GitLabCode_concatTest.c
+++ 5AIC0 code/GitLabCode_concatTest.c
@@ -1,15 +1,17 @@
 #include <stdlib.h>
-#include <stdio.h>    //Can we add this library?
+#include <stdio.h>
 #include <stdint.h>
-#include <xil_printf.h>
-#include <timers.h>
-#include "can_phy.h"
-#include "can_mac.h"
-
+#include <time.h>
 #define MAX_BIT_SIZE 135
-#define STUFFING_COUNT 6
-#define DEBUG 0         //Turn debugging on or off (extra prints etc.)
 
+typedef enum {DOMINANT = 0, RECESSIVE = 1, UNSPECIFIED = 2} CAN_SYMBOL;
+typedef enum {SYNC_SEG, PROP_SEG, PHASE_SEG_1, PHASE_SEG_2} BIT_SEGMENT;
+typedef struct _can_frame {
+  uint32_t ID;
+  uint32_t DLC;
+  uint64_t Data;
+  uint32_t CRC;
+} CAN_FRAME;
 
 CAN_SYMBOL * dec_to_bin(uint64_t number) {
     uint64_t temp[64];
@@ -76,56 +78,17 @@ CAN_SYMBOL * concatenate_binary(CAN_SYMBOL * bin1, CAN_SYMBOL * bin2) {
 void printFromPointer(CAN_SYMBOL * pointer) {
     int size = *pointer;
     for(int i = 1; i <= size; i++) {
-        printf("%d - ", *(pointer + i));
+        printf("%d", *(pointer + i));
     }
     printf("\n");
 }
 
-void sendBinaryStream(CAN_SYMBOL* stream) {
-    if (DEBUG) printf("Sending bitstream\n");
- 
-    CAN_SYMBOL last_bit = !stream[1];         //Start value != to first bit for successive counter
-    int successive = 1;
-    int max_index = stream[0] - 32;     //Max index to do bit stuffing to (exclude 32 bit CRC)
-    for (int i = 1; i <= stream[0]; i++) {
-        if (stream[i] == last_bit) successive++;
-        else successive = 1;
-        
-        if (i <= max_index && successive > STUFFING_COUNT) {        //Double check i<= max_index
-            can_phy_tx_symbol(!last_bit);
-             
-             if (DEBUG) {  
-                 printf(",");
-                if (last_bit) putchar('0');       
-                else putchar('1');                 
-                printf(",");
-             }
-             
-            last_bit = !last_bit;
-            successive = 1;
-            i--;                        //Decrease i to not skip the current bit in stream[i]
-            
-        } else {
-            can_phy_tx_symbol(stream[i]);
-            last_bit = stream[i];
-            
-            if (DEBUG) {
-                if (stream[i]) putchar('1');      //TEMP
-                else putchar('0');                 //TEMP
-            }
-            
-        }
-    }  
-}
+void can_mac_tx_frame(CAN_FRAME* txFrame){
+    // printFromPointer(dec_to_bin(txFrame->ID));
+    // printFromPointer(dec_to_bin(txFrame->DLC));
+    // printFromPointer(dec_to_bin(txFrame->Data));
+    // printFromPointer(dec_to_bin(txFrame->CRC));
 
-void can_mac_tx_frame(CAN_FRAME* txFrame) 
-{
-
-	printFromPointer(dec_to_bin(txFrame->ID));
-	printFromPointer(dec_to_bin(txFrame->DLC));
-	printFromPointer(dec_to_bin(txFrame->Data));
-	printFromPointer(dec_to_bin(txFrame->CRC));
-	
     CAN_SYMBOL* id_bin = dec_to_bin(txFrame->ID);
     CAN_SYMBOL* dlc_bin = dec_to_bin(txFrame->DLC);
     CAN_SYMBOL* data_bin = dec_to_bin(txFrame->Data);
@@ -133,14 +96,24 @@ void can_mac_tx_frame(CAN_FRAME* txFrame)
     CAN_SYMBOL* res =  concatenate_binary(id_bin, dlc_bin);
     CAN_SYMBOL* res2 =  concatenate_binary(res, data_bin);
     CAN_SYMBOL* res3 =  concatenate_binary(res2, crc_bin);
-    
-	printFromPointer(res3);
-    sendBinaryStream(res3);
-    
+    printFromPointer(res3);
 }
 
-void can_mac_rx_frame(CAN_FRAME* rxFrame)
-{
-  /* must be implemented by students */
-  
+int WinMain() {
+    CAN_FRAME frame = {10,20,43,52};
+    CAN_FRAME * frame_pointer = &frame;
+    // can_mac_tx_frame(frame_pointer);
+
+    srand(time(0));
+    int rand1, rand2, rand3, rand4;
+
+    for(int i = 0; i < 1000; i++){
+        frame.ID = rand() % (4294967295 + 1 - 0);
+        frame.DLC = rand() % (4294967295 + 1 - 0);
+        frame.Data = rand() % (18446744073709551615 + 1 - 0);
+        frame.CRC = rand() % (4294967295 + 1 - 0);
+        can_mac_tx_frame(frame_pointer);
+    }
+
 }
+
